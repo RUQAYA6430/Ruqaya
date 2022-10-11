bd3c40ff313ea8c1060942abb275b91eab0dda93
sfjbs <ruqayafathma09@gmail.com>
2022-10-11 05:14:34 +0000

    Spring Calculator

---
+++ Day04/spring-calculator/src/main/java/com/ruqaya/calculator/CalculatorController.java
@@ -0,0 +1,32 @@
+package com.rama.calculator;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public  class CalculatorController {
+
+    @GetMapping("/add")
+
+    public double addition(@RequestParam(name = "a", required = true) double a,@RequestParam(name = "b", required = true) double b) {
+        return a+b;
+    }
+
+    @GetMapping("/sub")
+    public double subtraction(@RequestParam(name = "a", required = true) double a,@RequestParam(name = "b", required = true) double b) {
+        return a-b;
+    }
+
+    @GetMapping("/mul")
+    public double multiplication(@RequestParam(name = "a", required = true) double a,@RequestParam(name = "b", required = true) double b) {
+        return a*b;
+    }
+
+    @GetMapping("/div")
+    public double division(@RequestParam(name = "a", required = true) double a,@RequestParam(name = "b", required = true) double b) {
+
+        return a/b;
+    }
+}
